d534165d678737ae3717dd9ccb9fa527ab81f041
Tomas Ekeberg <ekeberg@eduroam-1581.desy.de>
2017-05-17 14:59:04 +0200

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from distutils.core import setup, Extension
 import numpy.distutils.misc_util 
 
-# ext = Extension("nfft", sources=["nfftmodule.c"], libraries=["nfft3"], library_dirs=["/usr/local/lib"], include_dirs=["/usr/local/include", numpy.distutils.misc_util.get_numpy_include_dirs()])
-
-# setup(ext_modules=[ext])
-
-#ext = Extension("nfft", sources=["nfftmodule.c", "nfftclassmodule.c"], libraries=["nfft3"])
-ext = Extension("nfft", sources=["nfftmodule.c"], libraries=["nfft3"])
-setup(ext_modules=[ext], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
+ext = Extension("nfft", sources=["nfftmodule.c"], libraries=["nfft3"], include_dirs=[numpy.get_include(), "/usr/local/include"], library_dirs=["/usr/local/lib"])
+setup(ext_modules=[ext], include_dirs=[numpy.get_include(), "/usr/local/include"])
